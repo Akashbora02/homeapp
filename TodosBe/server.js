370844c0c473ba2c86642710aa337e6b7d05f34b
Akashbora02 <akashbora0082.com>
2026-01-10 13:51:01 +0530

    .env file updated

--- TodosBe/server.js
+++ TodosBe/server.js
@@ -1,3 +1,4 @@
+// TodosBe/server.js
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -6,6 +7,7 @@ import todoRoutes from "./routes/todoRoutes.js";
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
@@ -17,31 +19,8 @@ mongoose.connect(process.env.MONGO_URI)
 // Routes
 app.use("/todos", todoRoutes);
 
-// Fixed port from env
+// Fixed port from .env
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Todos backend running on port ${PORT}`);
 });
-
-
-
-
-
-/*import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import todoRoutes from "./routes/todoRoutes.js";
-
-dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-app.use("/todos", todoRoutes);
-app.listen(5000, () => console.log("Server running on port 5000"));
-*/
